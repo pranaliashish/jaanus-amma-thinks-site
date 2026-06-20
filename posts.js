02bb5ed8ea3ce393fa4f5001008e0256019ecd42
pranaliashish <pranaaliaashish@gmail.com>
2026-06-20 19:58:06 +0530

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,2 +1,76 @@
 const allPosts = [
-  ]
+
+   {
+        title: "The Little Firefly Who Wanted To Shine All Night",
+
+        category: "Bedtime Story",
+
+        description:
+            "A gentle bedtime story about rest, balance, and learning that even tiny lights need moments of pause to shine brightly.",
+
+        image: "firefly-story.png",
+
+        slug: "the-little-firefly-who-wanted-to-shine-all-night"
+    },
+
+    {
+    title: "The Tiny Spoon Who Wanted To Be A Sword",
+
+    category: "Bedtime Story",
+
+    description:
+        "A gentle bedtime story about discovering that true strength does not come from being powerful like others — but from embracing the quiet kindness we were made to give.",
+
+    image: "tiny-spoon-story.png",
+
+    slug: "the-tiny-spoon-who-wanted-to-be-a-sword"
+},
+
+    {
+    title: "The Little Cloud Who Held Everything Inside",
+
+    category: "Bedtime Story",
+
+    description:
+        "A gentle bedtime story about a little cloud who learns that sharing feelings can make the heart feel lighter. Perfect for helping children understand emotions, tears, and healthy emotional expression.",
+    image: "cloud-feelings-story.png",
+
+    slug: "the-little-cloud-who-held-everything-inside"
+},
+
+     {
+    title: "The Little Leaf Who Was Afraid To Fall",
+
+    category: "Bedtime Story",
+
+    description:
+        "A gentle bedtime story about change, courage, and learning to trust new beginnings. 🍂",
+    image: "leaf-story.png",
+
+    slug: "the-little-leaf-who-was-afraid-to-fall"
+},
+
+     {
+    title: "The Little Clock Who Wanted Time to Stop",
+
+    category: "Bedtime Story",
+
+    description:
+        "A gentle bedtime story about love, loss, healing, and the memories that stay with us. 🌙",
+    image: "clock-story.png",
+
+    slug: "the-little-clock-who-wanted-time-to-stop"
+},
+
+     {
+    title: "The Little Moon Who Was Afraid to Change 🌙",
+
+    category: "Bedtime Story",
+
+    description:
+        "A comforting bedtime story about a little moon who learns that changing doesn't make her less lovable. A gentle tale that helps children embrace growth, change, and self-worth.",
+    image: "moon-story.png",
+
+    slug: "the-little-moon-who-was-afraid-to-change-or-a-gentle-bedtime-story-about-growth-and-self-worth"
+}
+  ];
